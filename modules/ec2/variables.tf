cd030a6fb7faec63e0e5cc3b1ff60a7e030db1bd
Mahburat <makburatdevops@gmail.com>
2025-12-09 07:11:50 -0600

    Update variables.tf

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,14 +1,14 @@
+variable "project_name" {
+  type        = string
+  default     = "demo"
+}
+
 variable "ami" {
-  type    = string
-  default = "ami-0c02fb55956c7d316"
+  type        = string
+  default     = "ami-0c02fb55956c7d316"
 }
 
 variable "instance_type" {
-  type    = string
-  default = "t2.micro"
-}
-
-variable "name_prefix" {
-  type    = string
-  default = "demo"
+  type        = string
+  default     = "t2.micro"
 }
